de03c656180d83fcd3f4778176ef599daeaa8c64
FB-OLD <shenaittt9@gmail.com>
2026-06-25 12:54:34 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1691,10 +1691,8 @@ def check_key(key):
     try:
 
         headers = {
-
             "Cache-Control": "no-cache",
             "Pragma": "no-cache"
-
         }
 
         response = requests.get(
@@ -1710,30 +1708,40 @@ def check_key(key):
         ).decode()
 
         lines = content.splitlines()
+
         print("DEBUG: TOTAL LINES =", len(lines))
         print("DEBUG: INPUT KEY =", key)
 
         now = datetime.now()
 
         for line in lines:
-        print("DEBUG RAW LINE:", line)
 
-          line = line.strip()
+            print("DEBUG RAW LINE:", line)
 
-          if "|" not in line:
-              continue
+            line = line.strip()
 
-          saved_key, exp_date = line.split("|", 1)
-          print("DEBUG SAVED KEY:", saved_key.strip())
-          print("DEBUG EXP DATE:", exp_date.strip())
+            if "|" not in line:
+                continue
 
-          saved_key = saved_key.strip()
-          exp_date = exp_date.strip()
+            saved_key, exp_date = line.split("|", 1)
+
+            print("DEBUG SAVED KEY:", saved_key.strip())
+            print("DEBUG EXP DATE:", exp_date.strip())
+
+            saved_key = saved_key.strip()
+            exp_date = exp_date.strip()
 
             # MATCH KEY
             if saved_key.strip() == key.strip():
 
                 try:
+                    exp = datetime.strptime(
+                        exp_date,
+                        "%d-%m-%Y %I:%M %p"
+                    )
+
+                except:
+                    return "not", None, None
 # ================= LIVE CHECK KEY =================
 
 def check_key(key):
